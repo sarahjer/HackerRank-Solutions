2386ccbcf46bb8f757a11555f9c22f5a7cee3fbf
Sarah <sweetiejeremy@hotmail.com>
2017-04-24 14:28:45 -0700

    Changes

--- isPower.js
+++ isPower.js
@@ -1,3 +1,13 @@
+// Given a positive integer which fits in a 32 bit signed integer, find if it can be expressed as A^P where P > 1 and A > 0. A and P both should be integers.
+
+// Example
+
+// Input : 4
+// Output : True  
+// as 2^2 = 4. 
+
+
+
 isPower : function(A){
 	    if(A == 1) return 1;
         for(var base = 0; base <= (A/2); base++){
