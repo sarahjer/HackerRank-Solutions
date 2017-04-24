2e145a737807ec7456f14e6719badef45b0ecb65
Sarah <sweetiejeremy@hotmail.com>
2017-04-24 13:22:48 -0700

    Solution

---
+++ NodeValue.js
@@ -0,0 +1,25 @@
+/*
+    You’re given the pointer to the head node of a linked list and a specific position. Counting backwards from the tail node of the linked list, get the value of the node at the given position. A position of 0 corresponds to the tail, 1 corresponds to the node before the tail and so on.
+    
+    Get Nth element data from the end in a Linked List
+    Number of elements in the list will always be greater than N.
+    Node is defined as
+    var Node = function(data) {
+        this.data = data;
+        this.next = null;
+    }
+*/
+
+// This is a "method-only" submission.
+// You only need to complete this method.
+
+function getNodeValue( head, position) {
+    var current = head;
+    for(var i = 0; head.next != null; i++){
+        head = head.next;
+        if(i >= position){
+            current = current.next;
+        }        
+    }
+    return current.data;
+}
